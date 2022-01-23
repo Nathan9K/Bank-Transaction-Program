3beff5e24bd724d6a9a203e33d5754e085e2acb8
Nathan <97767955+Nathan9K@users.noreply.github.com>
2022-01-23 13:27:16 -0600

    add comments

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ Transaction Management
 
 #include <iostream>
 #include <string>
-#include <fstream>
+#include <fstream>		
 #include <sstream>
 #include "accounts.h"
 
@@ -22,32 +22,36 @@ using namespace std;
 
 int main()
 {	
-	string menuChoice;
-	bool menu = 0;
+	string menuChoice;	// stored user input for menu choice
+	bool menu = 0;		// declaration of boolean var for menu loop
 	
-	Accounts accounts;
+	Accounts accounts;	// class initialization of accounts
 	
 	cout << endl; cout << "Welcome to your Transaction Processing System" << endl;
 	cout << "Refer to the README file before using" << endl << endl;
 	
-	while (menu == 0)
+	while (menu == 0)	// condition for menu loop
 	{
 		cout << "Select from the list of options below" << endl;
 		
+		// menu options. user must only be able to enter any integer from 1-5
 		cout << "1. Add Account" << endl;
 		cout << "2. Remove Account" << endl;
 		cout << "3. Process Accounts" << endl;
 		cout << "4. Display Account Information" << endl;
 		cout << "5. Quit" << endl;
-
+		
+		// user enters choice
 		cout << "Enter your choice: "; cin >> menuChoice;
 		
-		if 		(menuChoice == "1") {accounts.addAccount(); menu = 0;}
+		// conditions for menu choices. if user doesn't enter a valid input, prompt to enter again and loop
+		// keep looping until the user decides to quit program
+		if 	(menuChoice == "1") {accounts.addAccount(); menu = 0;}
 		else if (menuChoice == "2") {accounts.removeAccount(); menu = 0;}
 		else if (menuChoice == "3") {accounts.getBalance(); menu = 0;}
 		else if (menuChoice == "4") {accounts.displayAccount(); menu = 0;}
 		else if (menuChoice == "5") {cout << endl << "Exiting ..." << endl; menu = 1;}	
-		else 						{cout << "Not a valid option, please enter again..." << endl; menu = 0;}	
+		else 	{cout << "Not a valid option, please enter again..." << endl; menu = 0;}	
 	}
 	
 	return 0;
